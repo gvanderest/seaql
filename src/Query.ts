cc4e3a84ac35bb02d7143b67522808df587e3e42
Guillaume VanderEst <guillaume@vanderest.org>
2018-10-17 22:32:27 -0700

    feat(Query): rename class and add jsdoc

--- src/Query.ts
+++ src/Query.ts
@@ -1,5 +1,5 @@
 type Action = "select" | "insert" | "update" | "delete";
-type Value = string | number | boolean;
+type Value = string | number | boolean | null;
 type Data = {
     [key: string]: Value;
 };
@@ -16,13 +16,26 @@ interface ISettings {
     table?: Table;
 }
 
-export default class SeaQL {
+/**
+ * Query which can be manipulated with functions calls and chaining.
+ */
+export default class Query {
     protected action: Action;
     protected data: Data = {};
     protected fields: Fields = [];
     protected table: Table;
     protected whereClauses: WhereClauses = {};
 
+    /**
+     * Create a SeaQL instance, which typically involves setting its action
+     * and basic fields or table depending on action.
+     *
+     * @param settings
+     * @see insert
+     * @see select
+     * @see update
+     * @see delete
+     */
     constructor(settings: ISettings) {
         Object.keys(settings).forEach((key) => {
             const value = settings[key];
@@ -30,59 +43,115 @@ export default class SeaQL {
         });
     }
 
-    static select(fields) {
-        return new SeaQL({
+    /**
+     * Start a select query.
+     *
+     * @param fields being queried from a table
+     * @see from
+     * @see where
+     */
+    static select(fields: string[]): Query {
+        return new Query({
             action: "select",
             fields,
         });
     }
 
+    /**
+     * Start a delete query.
+     * Because `delete` is a reserved JavaScript keyword, it is named `remove`.
+     *
+     * @param table containing records that are being deleted
+     * @see where
+     */
     static remove(table) {
-        return new SeaQL({
+        return new Query({
             action: "delete",
             table,
         });
     }
 
-    static del(table) {
-        return SeaQL.remove(table);
+    /**
+     * Alias for the `remove` function, to start a DELETE query.
+     *
+     * @see remove
+     */
+    static del(table): Query {
+        return Query.remove(table);
     }
 
-    static update(table) {
-        return new SeaQL({
+    /**
+     * Start an update query.
+     *
+     * @param table being updated
+     * @see set
+     * @see where
+     */
+    static update(table): Query {
+        return new Query({
             action: "update",
             table,
         });
     }
 
+    /**
+     * Specify the table being selected from.
+     *
+     * @see select
+     * @see update
+     * @see delete
+     */
     public from(table) {
         this.table = table;
         return this;
     }
 
+    /**
+     * Specify the table being inserted into.
+     *
+     * @see insert
+     */
     public into(table) {
         this.table = table;
         return this;
     }
 
+    /**
+     * Limit with clauses to filter affected records.
+     *
+     * @param whereClauses { field1: 'value1', field2: 12345, field3: true }
+     */
     public where(whereClauses: WhereClauses) {
         this.whereClauses = whereClauses;
         return this;
     }
 
+    /**
+     * Proide data to an update query.
+     *
+     * @param data { field1: 'value1', field2: 12345, field3: true }
+     */
     public set(data: Data) {
         this.data = data;
         return this;
     }
 
-    static insert(data) {
-        return new SeaQL({
+    /**
+     * Start an insert query.
+     *
+     * @see into
+     */
+    static insert(data): Query {
+        return new Query({
             action: "insert",
             data,
         });
     }
 
-    public stringify() {
+    /**
+     * Convert the Query object into an SQL string.
+     */
+    public stringify(): string {
         switch (this.action) {
             case "insert": {
                 return this.formatInsertSql();
@@ -99,17 +168,32 @@ export default class SeaQL {
         }
     }
 
-    private generateWhereClauseSql(whereClauses) {
+    /**
+     * Convert the Object that makes up where clauses into a joined string.
+     *
+     * @param whereClauses { field1: 'value1', field2: 12345, field3: true }
+     * @see stringify
+     */
+    private generateWhereClauseSql(whereClauses): string {
         const sql: string[] = [];
         Object.keys(this.whereClauses).forEach((key) => {
             const value = this.whereClauses[key];
-            sql.push(`${key} = ${this.formatValue(value)}`);
+            if (value === null) {
+                sql.push(`${key} IS NULL`);
+            } else {
+                sql.push(`${key} = ${this.formatValue(value)}`);
+            }
         });
 
         return sql.join(" AND ");
     }
 
-    public formatSelectSql() {
+    /**
+     * Output a select query.
+     *
+     * @see select
+     */
+    public formatSelectSql(): string {
         const parts: string[] = [];
 
         parts.push(`SELECT ${this.fields.join(", ")}`);
@@ -119,7 +203,12 @@ export default class SeaQL {
         return parts.join(" ");
     }
 
-    public formatInsertSql() {
+    /**
+     * Output an insert query.
+     *
+     * @see insert
+     */
+    public formatInsertSql(): string {
         const parts: string[] = [];
 
         const fields = Object.keys(this.data);
@@ -133,7 +222,12 @@ export default class SeaQL {
         return parts.join(" ");
     }
 
-    public formatDeleteSql() {
+    /**
+     * Output a delete query.
+     *
+     * @see delete
+     */
+    public formatDeleteSql(): string {
         const parts: string[] = [];
 
         parts.push(`DELETE FROM ${this.table}`);
@@ -142,7 +236,12 @@ export default class SeaQL {
         return parts.join(" ");
     }
 
-    public formatUpdateSql() {
+    /**
+     * Output an update query.
+     *
+     * @see update
+     */
+    public formatUpdateSql(): string {
         const parts: string[] = [];
 
         const updatesSql = Object.keys(this.data).map((key) => {
@@ -157,18 +256,25 @@ export default class SeaQL {
         return parts.join(" ");
     }
 
-    protected formatValue(value) {
+    /**
+     * Take a value and output it into an SQL friendly format.
+     *
+     * @todo Escape values
+     */
+    protected formatValue(value): string {
         // TODO: Implement this.
         if (typeof value === "boolean" || typeof value === "number") {
             return String(value);
         } else if (typeof value === "string") {
             return `'${value}'`;
+        } else if (value === null) {
+            return 'NULL';
         }
     }
 }
 
-export const insert = SeaQL.insert;
-export const select = SeaQL.select;
-export const update = SeaQL.update;
-export const remove = SeaQL.remove;
-export const del = SeaQL.del;
+export const insert = Query.insert;
+export const select = Query.select;
+export const update = Query.update;
+export const remove = Query.remove;
+export const del = Query.del;
